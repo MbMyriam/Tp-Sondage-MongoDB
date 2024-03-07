2b1fc0ea1adb7e2c7db27144749e8f397f596811
MbMyriam <myriam.mb@ynov.com>
2024-03-07 09:52:33 +0100

    Ajout du RegisterForm dans le App

--- imports/client/App.jsx
+++ imports/client/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { LoginForm } from './components/LoginForm';
+import { RegisterForm } from './components/RegisterForm';
 
 export const App = () => (
   <div>
     <h1>Tp Sondage MongoDB!</h1>
+    <RegisterForm/>
     <LoginForm/>
   </div>
   
