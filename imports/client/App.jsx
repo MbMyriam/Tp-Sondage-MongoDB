bb7750df69b08e82d51652bc3fca0065e58f85d1
MbMyriam <myriam.mb@ynov.com>
2024-03-07 12:15:42 +0100

    Ajout component SondageForm dans App

--- imports/client/App.jsx
+++ imports/client/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { LoginForm } from './components/LoginForm';
 import { RegisterForm } from './components/RegisterForm';
+import { SondageForm } from './components/SondageForm';
 
 export const App = () => (
   <div>
     <h1>Tp Sondage MongoDB!</h1>
     <RegisterForm/>
+    <br/>
     <LoginForm/>
+    <br/>
+    <SondageForm/>
   </div>
   
 );
