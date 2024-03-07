437190ef9f35883c3611fb3cacf96bc74052dfd6
MbMyriam <myriam.mb@ynov.com>
2024-03-07 09:41:15 +0100

    Import LoginForm dans le App

--- imports/client/App.jsx
+++ imports/client/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { LoginForm } from './components/LoginForm';
 
 export const App = () => (
   <div>
     <h1>Tp Sondage MongoDB!</h1>
+    <LoginForm/>
   </div>
+  
 );
