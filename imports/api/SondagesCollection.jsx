42fa2a998dc5905e73a52564fe4409909d4f1f74
MbMyriam <myriam.mb@ynov.com>
2024-03-07 10:57:43 +0100

    Ajout des méthodes pour insérer et publier des sondages

--- imports/api/SondagesCollection.jsx
+++ imports/api/SondagesCollection.jsx
@@ -48,6 +48,36 @@ const SondagesSchema = new SimpleSchema({
 
 SondagesCollection.attachSchema(SondagesSchema);
 
+// Exécuté uniquement sur le serveur
+if (Meteor.isServer) {
+    Meteor.publish('allSondagesCollection', function () {
+        // Renvoi tous les sondages de la collection
+        return SondagesCollection.find();
+    });
+
+    Meteor.publish('oneSondagesCollection', (id) => {
+        return SondagesCollection.find({ _id: id });
+    });
+
+    Meteor.publish('mesSondagesCollection', function () {
+        return SondagesCollection.find({ 'sondage.createur': this.userId })
+    });
+
+    //Méthode côté serveur qu'on peut appeler côté client insert
+    Meteor.methods({
+        insertNewSondage(nomSondage, sondageCreateurId, questionsSondage) {
+            SondagesCollection.insert({
+                sondage: {
+                    nom: nomSondage,
+                    createur: sondageCreateurId,
+                    questions: questionsSondage,
+                    createdAt: new Date(),
+                }
+            });
+        },
+    })
+}
+
 
 
 
