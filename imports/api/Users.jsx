80ec01dc09b943856fffc9cbc3e13a4697dba397
MbMyriam <myriam.mb@ynov.com>
2024-03-07 09:36:17 +0100

    Ajout des méthodes users

--- imports/api/Users.jsx
+++ imports/api/Users.jsx
@@ -1,1 +1,23 @@
-import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+
+
+if (Meteor.isServer) {
+
+    Meteor.publish('allUsers', function () {
+        return Meteor.users.find({});
+    })
+
+    Meteor.publish('currentUser', function () {
+        return Meteor.user();
+    })
+
+    Meteor.publish('find_by_username', function (name) {
+        return Accounts.findUserByUsername(name);
+    })
+
+    Meteor.publish('users.all', () => {
+        return [
+            Meteor.users.find()
+        ];
+    });
+}
